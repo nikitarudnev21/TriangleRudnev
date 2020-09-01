f91eb90827bd7047cef8f920d2e2ad5b21cc5ef0
Opilane TTHK <opilane@tthk.ee>
2020-09-01 11:36:27 +0300

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,72 @@
+﻿namespace TriangleRudnev
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.btnStart = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnStart
+            // 
+            this.btnStart.BackColor = System.Drawing.Color.SaddleBrown;
+            this.btnStart.Cursor = System.Windows.Forms.Cursors.WaitCursor;
+            this.btnStart.FlatAppearance.BorderColor = System.Drawing.Color.Orange;
+            this.btnStart.FlatAppearance.BorderSize = 3;
+            this.btnStart.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnStart.Font = new System.Drawing.Font("Segoe Print", 24.75F);
+            this.btnStart.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.btnStart.Location = new System.Drawing.Point(601, 39);
+            this.btnStart.Margin = new System.Windows.Forms.Padding(0);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(160, 68);
+            this.btnStart.TabIndex = 0;
+            this.btnStart.Text = "Запуск";
+            this.btnStart.UseVisualStyleBackColor = false;
+            this.btnStart.UseWaitCursor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.PeachPuff;
+            this.ClientSize = new System.Drawing.Size(784, 361);
+            this.Controls.Add(this.btnStart);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Работа с треугольником";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnStart;
+    }
+}
+
---
+++ Triangle.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TriangleRudnev
+{
+    class Triangle
+    {
+        public double a;
+        public double b;
+        public double c;
+        public Triangle(double A, double B, double C)
+        {
+            a = A;
+            b = B;
+            c = C;
+        }
+        public string outputA()
+        {
+            return Convert.ToString(a);
+        }
+        public string outputB()
+        {
+            return Convert.ToString(b);
+        }
+        public string outputC()
+        {
+            return Convert.ToString(c);
+        }
+        public double Perimeter()
+        {
+            return a + b + c;
+        }
+        public double Surface()
+        {
+            double p = (a+b+c) /2;
+            return Math.Sqrt((p * (p - a) * (p - b) * (p - c)));
+        }
+        public double GetSetA
+        {
+            get{ return a; }
+            set{ a = value; }
+        }
+        public double GetSetB
+        {
+            get { return b; }
+            set { b = value; }
+        }
+        public double GetSetC
+        {
+            get { return c; }
+            set { c = value; }
+        }
+        public bool ExistTriangle
+        {
+            get
+            {
+                if ((a > b + c) && (b > a + c) && (c > a + b)) return false;
+                else return true;
+            }
+        }
+    }
+
+}
